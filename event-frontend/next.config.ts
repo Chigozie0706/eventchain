d981295ae5beb454a57cdf1bb6971f4a4f6a6282
chigozie0706 <chigoziejacob@gmail.com>
2025-06-20 03:27:32 +0100

    checked next config

--- event-frontend/next.config.ts
+++ event-frontend/next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   async headers() {
-    return [{
-      source: '/api/:path*',
-      headers: [
-        { 
-          key: 'Access-Control-Allow-Origin',
-          value: 'https://e4cd-102-88-115-145.ngrok-free.app/' 
-        },
-        { key: 'Access-Control-Allow-Methods', value: 'POST, OPTIONS' }
-      ]
-    }];
-  },
   /* config options here */
   eslint: {
     // Warning: This allows production builds to successfully complete even if
